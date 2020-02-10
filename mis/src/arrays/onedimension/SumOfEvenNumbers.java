70cd9825b5080548e4a14db0c33f44028dd73ca9
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-10 12:27:45 -0600

    Create SumOfEvenNumbers.java
    - Program to find sum of all even numbers in array.

---
+++ mis/src/arrays/onedimension/SumOfEvenNumbers.java
@@ -0,0 +1,33 @@
+/**
+ * Program to find sum of all even numbers in array.
+ */
+package arrays.onedimension;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+
+	public static void main(String[] args) {
+		int arrSize;
+		int evenNumberSum = 0;
+		System.out.println("Enter array size:");
+		Scanner scanner = new Scanner(System.in);
+		arrSize = scanner.nextInt();
+		int arr[] = new int[arrSize];
+		
+		System.out.println("Enter array values:");
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] % 2 == 0) {
+				evenNumberSum += arr[i];
+			}
+		}
+		
+		System.out.println("Sum of even numbers: "+evenNumberSum);
+
+	}
+
+}
