a5013df7bd3cb797fc05aa3ad759f92cca880fb4
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-14 12:01:04 -0600

    Update OrderProcessing.java
    - Capturing customer details, order details, product details, stock details

--- mis/src/project/order/processing/OrderProcessing.java
+++ mis/src/project/order/processing/OrderProcessing.java
@@ -1,14 +1,17 @@
 package project.order.processing;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class OrderProcessing {
 
 	public static void main(String[] args) {
 		
-		// Entering customer details
 		Scanner sci = new Scanner(System.in);
 		Scanner scs = new Scanner(System.in);
+		Scanner scf = new Scanner(System.in);
+		
+		// Entering customer details
 		System.out.println("Enter customerId, customerName, address, phone:");
 		int customerId, phone;
 		String customerName, address;
@@ -17,8 +20,48 @@ public class OrderProcessing {
 		address = scs.nextLine();
 		phone = sci.nextInt();
 		
-		Customer cust = new Customer(customerId, customerName, address, phone);
-		System.out.println(cust);
+		Customer customer = new Customer(customerId, customerName, address, phone);
+		System.out.println(customer);
+		
+		//Entering order details
+		System.out.println("Enter orderId, customerId, customerName, productId, amount, orderDate");
+		int orderId, productId;
+		Date orderDate;
+		float amount;
+		orderId = sci.nextInt();
+		customerId = sci.nextInt();
+		customerName = scs.nextLine();
+		productId = sci.nextInt();
+		amount = scf.nextFloat();
+		orderDate = new Date(scs.nextLine());
+		
+		Order order = new Order(orderId, customerId, customerName, productId, amount, orderDate);
+		System.out.println(order);
+		
+		//Entering product details
+		System.out.println("Enter productId, productPrice and productType");
+		float productPrice;
+		String productType;
+		productId = sci.nextInt();
+		productPrice = scf.nextFloat();
+		productType = scs.nextLine();
+		
+		Product product = new Product(productId, productPrice, productType);
+		System.out.println(product);
+		
+		//Entering stock details
+		System.out.println("Enter productId, quantity, shopNo");
+		int quantity, shopNo;
+		productId = sci.nextInt();
+		quantity = sci.nextInt();
+		shopNo = sci.nextInt();
+		
+		Stock stock = new Stock(productId, quantity, shopNo);
+		System.out.println(stock);
+		
+		sci.close();
+		scs.close();
+		scf.close();
 		
 	}
 
