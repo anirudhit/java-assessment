b7c9eb6b1f154b37353938d1a79eab9309df7475
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-17 15:46:44 -0600

    Create BasicExceptions
    - unchecked exceptions implementation

---
+++ mis/src/exception/impl/BasicExceptions.java
@@ -0,0 +1,26 @@
+/*
+ * Basic exception implementation
+ */
+
+package exception.impl;
+
+public class BasicExceptions {
+
+	public static void main(String[] args) {
+		
+		String str = "Dallas";
+		String msg = null;
+		int arr[] = {1,2,3,4,5};
+		
+		//StringIndexOutOfBoundsException
+		//System.out.println(str.charAt(10));
+		
+		//NullPointerException
+		//System.out.println(msg.length());
+		
+		//ArrayIndexOutOfBoundsException
+		//System.out.println(arr[19]);
+
+	}
+
+}
