8c7c3a9285c624eebea68ea4a14205dfdb20521e
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-17 19:27:29 -0600

    Create MultiThreadLambda.java
    - Multi thread functionality using lambda function in thread, implementing join function such that main function waits and checking the alive status of the thread.

---
+++ mis/src/thread/impl/multi/MultiThreadLambda.java
@@ -0,0 +1,38 @@
+/*
+ * Multi thread functionality using lambda function in thread, implementing join function such that main function
+ * waits and checking the alive status of the thread.
+ */
+package thread.impl.multi;
+
+public class MultiThreadLambda {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		Thread t1 = new Thread(() -> {
+			for(int i=1; i<=5; i++) {
+				System.out.println("Thread-1: "+i);
+			}
+		});
+		
+		Thread t2 = new Thread(() -> {
+			for(int i=1; i<=5; i++) {
+				System.out.println("Thread-2: "+i);
+			}
+		});
+		
+		t1.start();
+		System.out.println("Alive status of thread1: "+t1.isAlive());
+		
+		t2.start();
+		System.out.println("Alive status of thread2: "+t2.isAlive());
+		
+		t1.join();
+		t2.join();
+		System.out.println("Bye");
+		
+		System.out.println("Alive status of thread1: "+t1.isAlive());
+		System.out.println("Alive status of thread2: "+t2.isAlive());
+		
+	}
+
+}
