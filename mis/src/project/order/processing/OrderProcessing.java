a1ca12c4e2f7b68a891209dd4398c6a5477f2f7c
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-19 09:22:59 -0600

    Update OrderProcessing.java
    - Added supress warning annotation

--- mis/src/project/order/processing/OrderProcessing.java
+++ mis/src/project/order/processing/OrderProcessing.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class OrderProcessing {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		
 		Scanner sci = new Scanner(System.in);
