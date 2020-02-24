a09c4a080d77abb80bc8378abb9db4a312c167b0
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-24 00:53:33 -0600

    Create RedisDemo.java
    - created a redis client
    - list and set operations usage

---
+++ mis/src/redis/impl/RedisDemo.java
@@ -0,0 +1,31 @@
+package redis.impl;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisDemo {
+
+	public static void main(String[] args) {
+		
+		String host = "localhost";
+		
+		Jedis jedis = new Jedis(host);
+		System.out.println("Connection successful");
+		System.out.println("Server ping: "+jedis.ping());
+		System.out.println("Server info: "+jedis.info());
+		
+		//Setting List with key and value
+		System.out.println("List push with key,value: "+jedis.lpush("apple", "red"));
+		jedis.lpush("apple", "red");
+		
+		//Fetching List value with key
+		System.out.println("List pop with key(apple): "+ jedis.lpop("apple"));
+		
+		//Setting Set with key and value
+		System.out.println("Set push with key,value: "+jedis.sadd("lemon", "yellow"));
+		jedis.lpush("apple", "red");
+		
+		//Fetching Set value with key
+		System.out.println("Set pop with key(lemon): "+ jedis.spop("lemon"));
+	}
+
+}
