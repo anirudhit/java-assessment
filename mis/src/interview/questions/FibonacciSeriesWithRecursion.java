db8dc12d776bed00e3dad9d1843fc9685ec3a7f7
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-24 23:28:58 -0600

    Create FibonacciSeriesWithRecursion.java
    - Fibonacci series with recursion

---
+++ mis/src/interview/questions/FibonacciSeriesWithRecursion.java
@@ -0,0 +1,31 @@
+/*
+ * Fibonacci series with recursion
+ */
+package interview.questions;
+import java.util.Scanner;
+
+public class FibonacciSeriesWithRecursion {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value: ");
+		n = sc.nextInt();
+		
+		for(int i = 0;i<n;i++) {
+			System.out.println(fibSeries(i));
+		}
+		sc.close();
+	}
+	
+	public static int fibSeries(int n) {
+		if(n == 0) {
+			return 0;
+		}else if(n == 1) {
+			return 1;
+		}else {
+			return fibSeries(n-1)+ fibSeries(n-2);
+		}
+	}
+
+}
