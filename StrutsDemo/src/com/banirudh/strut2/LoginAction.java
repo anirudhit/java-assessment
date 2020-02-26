0c2004605ce596728487af0545a93c309b2d3793
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-26 14:22:04 -0600

    StrutsDemo
    - Initialized struts demo application

---
+++ StrutsDemo/src/com/banirudh/strut2/LoginAction.java
@@ -0,0 +1,15 @@
+package com.banirudh.strut2;
+
+//import org.apache.struts2.dispatcher.ng.filter.StrutsPrepareAndExecuteFilter;
+
+import com.opensymphony.xwork2.Action;
+
+public class LoginAction implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return ERROR;
+	}
+
+}
