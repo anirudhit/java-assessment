439573d94e466360f75d67ee8025e34c2cd95885
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-26 23:59:50 -0600

    Update MatrixTranspose.java
    - Matrix transpose using swapping

--- mis/src/interview/questions/MatrixTranspose.java
+++ mis/src/interview/questions/MatrixTranspose.java
@@ -1,3 +1,7 @@
+/*
+ * Matrix transpose using swapping
+ */
+
 package interview.questions;
 
 import java.util.Scanner;
@@ -18,7 +22,27 @@ public class MatrixTranspose {
 			}
 		}
 		
+		// Printing the arr matrix
 		System.out.println("Values in arr["+n+"]"+"["+n+"] :");
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print(arr[i][j] +"\t");
+			}
+			System.out.print("\n");
+		}
+		
+		// Matrix transpose logic
+		int temp;
+		for(int i=0;i<n;i++) {
+			for(int j=i;j<n;j++) {
+				temp  = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = temp;
+			}
+		}
+		
+		// Printing the arr matrix transpose
+		System.out.println("Transpose of arr["+n+"]"+"["+n+"] :");
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
 				System.out.print(arr[i][j] +"\t");
