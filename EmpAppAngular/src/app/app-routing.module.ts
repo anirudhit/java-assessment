47f0f54c8142f6c0549de8373413cc5ddddf842e
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-01 10:40:45 -0600

    Update EmpAngularApp
    - Added routes to the application

--- EmpAppAngular/src/app/app-routing.module.ts
+++ EmpAppAngular/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ComponentListComponent } from './component-list/component-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'components', component: ComponentListComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  HeaderComponent,
+  FooterComponent,
+  HomeComponent,
+  ComponentListComponent
+];
