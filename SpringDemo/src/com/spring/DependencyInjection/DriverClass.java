68a4068912925c20221ac4635571fd083e226877
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-04 22:13:39 -0600

    Update SpringDemo
    - Added skill list attribute
    - Added application context config for skill list

--- SpringDemo/src/com/spring/DependencyInjection/DriverClass.java
+++ SpringDemo/src/com/spring/DependencyInjection/DriverClass.java
@@ -6,7 +6,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DriverClass {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Printing developer object with empId, empName
 		Developer developerObject = (Developer)context.getBean("developerObject");
 		System.out.println(developerObject);
+		
+		// Printing project object with projectName, projectId
+		Project projectObject = (Project)context.getBean("projectObject");
+		System.out.println(projectObject);
+		
+		// Printing project object with skill
+		Project projectObjectSkill = (Project)context.getBean("projectObjectSkill");
+		System.out.println(projectObjectSkill);
 	}
 }
--- SpringDemo/src/com/spring/DependencyInjection/Project.java
+++ SpringDemo/src/com/spring/DependencyInjection/Project.java
@@ -1,18 +1,22 @@
 package com.spring.DependencyInjection;
 
+import java.util.List;
+
 public class Project {
 	private String projectName;
 	private int projectId;
+	private List<String> skills;
 	
 	public Project(String projectName, int projectId) {
-		super();
 		this.projectName = projectName;
 		this.projectId = projectId;
 	}
-
+	public Project(List<String> skills) {
+		this.skills = skills;
+	}
 	@Override
 	public String toString() {
-		return "Project [projectName=" + projectName + ", projectId=" + projectId + "]";
+		return "Project [projectName=" + projectName + ", projectId=" + projectId + ", skills=" + skills + "]";
 	}
 	
 }
