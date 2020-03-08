414477d78865fbcee11d550b199ddd2425bb2067
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-08 17:15:00 -0500

    Update SpringFeatures
    - Injecting properties file values into beans.xml
    - Added <context:property-placeholder /> tag in beans.xml and configured the student.info.properties location

---
+++ SpringFeatures/src/com/banirudh/student/spring/injecting/properties/xml/Student.java
@@ -0,0 +1,23 @@
+package com.banirudh.student.spring.injecting.properties.xml;
+
+public class Student {
+	private String name;
+	private String interestedCourse;
+	private String hobby;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setInterestedCourse(String interestedCourse) {
+		this.interestedCourse = interestedCourse;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	
+	public void displayStudentInfo() {
+		System.out.println("Student name: "+name);
+		System.out.println("Student interested course: "+interestedCourse);
+		System.out.println("Student hobby: "+hobby);
+	}
+}
