8968cf549a42b50d903ecdd0ce640727cfb4d46a
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-07 19:11:05 -0600

    update SpringFeatures
    - Updated classpath, added spring libraries in build path
    - Updated display message in Vodafone class

--- SpringFeatures/src/com/banirudh/sim/polymorphism/Vodafone.java
+++ SpringFeatures/src/com/banirudh/sim/polymorphism/Vodafone.java
@@ -4,12 +4,12 @@ public class Vodafone implements Sim {
 
 	@Override
 	public void calling() {
-		System.out.println("Calling from Airtel Sim.");
+		System.out.println("Calling from Vodafone Sim.");
 	}
 
 	@Override
 	public void data() {
-		System.out.println("Data from Airtel Sim.");
+		System.out.println("Data from Vodafone Sim.");
 	}
 
 }
