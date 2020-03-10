cf749e4969d354c465828073061a435e15553493
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-09 21:38:59 -0500

    Update SpringFeatures
    - Added aliases to @Bean with name attribute @Bean(name = {"collegeObj", "anotherCollegeObj"})
    - Updated bean name in the client class

--- SpringFeatures/src/com/banirudh/college/spring/annotations/CollegeConfig.java
+++ SpringFeatures/src/com/banirudh/college/spring/annotations/CollegeConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CollegeConfig {
-	@Bean
+	@Bean(name = {"collegeObj", "anotherCollegeObj"})
 	public College collegeObject() {
 		College college = new College();
 		return college;
