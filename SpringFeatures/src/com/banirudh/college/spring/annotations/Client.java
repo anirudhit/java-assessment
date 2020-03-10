ab2667e96ebcf672ebee32ff90e1215cf8e811dc
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-09 20:12:51 -0500

    Update SpringFeatures
    - Deleted beans.xml
    - Created CollegeConfig class with @configuration and @componentScan annotations which made the beans.xml configuration
    - Loading configuration in Client with "AnnotationConfigApplicationContext"

--- SpringFeatures/src/com/banirudh/college/spring/annotations/Client.java
+++ SpringFeatures/src/com/banirudh/college/spring/annotations/Client.java
@@ -1,12 +1,12 @@
 package com.banirudh.college.spring.annotations;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/banirudh/college/spring/annotations/beans.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
 		System.out.println("Beans loaded...");
 		College collegeObject = context.getBean("collegeBean", College.class);
 		System.out.println("College object created: "+collegeObject);
---
+++ SpringFeatures/src/com/banirudh/college/spring/annotations/CollegeConfig.java
@@ -0,0 +1,9 @@
+package com.banirudh.college.spring.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.banirudh.college.spring.annotations")
+public class CollegeConfig {
+}
