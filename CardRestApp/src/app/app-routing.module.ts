9e6f52e15126f8202d68a9eea6b1c93cd3720b17
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-19 21:12:26 -0500

    Update CardRestTransactions
    - Added routes to the components : account, dashboard, home, transaction-history, user

--- CardRestApp/src/app/app-routing.module.ts
+++ CardRestApp/src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountComponent } from './account/account.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { UserComponent } from './user/user.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'transaction-history', component: TransactionHistoryComponent},
+  {path: 'user', component: UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RoutingComponents = [
+  HomeComponent,
+  DashboardComponent,
+  AccountComponent,
+  TransactionHistoryComponent,
+  UserComponent
+];
--- CardRestApp/src/app/app.module.ts
+++ CardRestApp/src/app/app.module.ts
@@ -2,24 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AccountComponent } from './account/account.component';
-import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
-import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent,
-    DashboardComponent,
-    AccountComponent,
-    TransactionHistoryComponent,
-    UserComponent
+    RoutingComponents
   ],
   imports: [
     BrowserModule,
