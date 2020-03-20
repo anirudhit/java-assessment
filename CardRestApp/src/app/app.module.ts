e22f4429100c6aa709cd447342fe24f55485bfa8
anirudh bellamkonda <anirudhit@outlook.com>
2020-03-19 20:34:13 -0500

    Update CardRestTransactions
    - Generated components : account, dashboard, home, transaction-history, user

--- CardRestApp/src/app/app.module.ts
+++ CardRestApp/src/app/app.module.ts
@@ -5,11 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountComponent } from './account/account.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    DashboardComponent,
+    AccountComponent,
+    TransactionHistoryComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
